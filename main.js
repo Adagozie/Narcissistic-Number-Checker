de06debdd80eac710bf70be078419c78486cb454
Adagozie <adagozie1@gmail.com>
2021-09-17 22:38:28 -0700

    initial commit

---
+++ main.js
@@ -0,0 +1,21 @@
+var submit = document.getElementById ("submit");
+
+submit.onclick = function(){	
+ var userinput = document.getElementById ("userinput").value;
+ 
+ document.getElementById ("output");
+
+}
+
+function narcissistic(userinput) {
+ var exponent = (''+userinput).length;
+console.log(exponent);
+				
+}
+
+
+
+
+/*var int_number = userinput; var int_length = (''+userinput).length;
+console.log(int_length);*/
+
